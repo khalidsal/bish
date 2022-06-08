662f84f0fff061cb144973dbf34019b29316498c
khalidsal <106035905+khalidsal@users.noreply.github.com>
2022-06-08 23:31:38 +0100

    Update EditListingDeliveryForm.js

--- src/containers/EditListingPage/EditListingWizard/EditListingDeliveryPanel/EditListingDeliveryForm.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingDeliveryPanel/EditListingDeliveryForm.js
@@ -250,7 +250,7 @@ export const EditListingDeliveryFormComponent = props => (
             name="deliveryOptions"
             label={shippingLabel}
             value="shipping" 
-          />
+          
           </div> 
               
           <Button
