47a32e06315883d5c53288867c36b5eec2f2cac7
khalidsal <106035905+khalidsal@users.noreply.github.com>
2022-06-23 10:40:18 +0100

    Update EditListingDeliveryForm.js

--- src/containers/EditListingPage/EditListingWizard/EditListingDeliveryPanel/EditListingDeliveryForm.js
+++ src/containers/EditListingPage/EditListingWizard/EditListingDeliveryPanel/EditListingDeliveryForm.js
@@ -196,6 +196,13 @@ export const EditListingDeliveryFormComponent = props => (
               // See example: https://codesandbox.io/s/changing-field-level-validators-zc8ei
               key={shippingEnabled ? 'oneItemValidation' : 'noOneItemValidation'}
             />
+            <FieldCheckbox
+              id="shipping"
+              className={css.deliveryCheckbox}
+              name="deliveryOptions"
+              label={shippingLabel}
+              value="shipping"
+          />
           </div>
 
           <Button
